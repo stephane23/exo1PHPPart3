ee2a43575e28bcf74357cc5b79fa1c6860bc9d97
stephane23 <stephane.fernandes@et.simplon-roanne.com>
2018-07-23 16:53:35 +0200

    Part3

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+
+// exo 1
+for ($boucle = 0; $boucle < 10; $boucle ++) {
+        echo $boucle . '<br />';
+}
+echo "<br>";
+
+//exo2
+$first = 0;
+$second = rand (1, 100);
+for ($first = 0; $first < 20; $first++) {
+    echo $result = $first * $second ."<br>";
+}
+
+echo "<br>";
+
+//exo3
+$fourth = rand (1, 100);
+for ($third = 100; $third >= 10; $third--) {
+    echo $result2 = $third * $fourth . "<br>";
+}
+
+echo "<br>";
+
+//exo4
+for ($ini = 1; $ini <= 10;) {
+    echo "<br>" . $ini;
+    $ini += $ini/2;
+}
+
+echo "<br>";
+
+//exo5
+for ($ini2 = 1; $ini2 <= 15; $ini2++) {
+    echo "On y arrive presque" . "<br>";
+}
+
+echo "<br>";
+
+//exo6
+for ($ini3 = 20; $ini3 >= 0; $ini3--) {
+    echo "C'est presque bon" ."<br>";
+}
+
+echo "<br>";
+
+//exo7
+for ($ini4 = 1; $ini4 <= 100; $ini4+=15) {
+    echo "On tient le bon bout" ."<br>";
+}
+
+echo "<br>";
+
+//exo8
+for ($ini5 = 200; $ini5 >= 0; $ini5-=12) {
+    echo "Enfin !!!!" ."<br>";
+}
+
+echo "<br>";
+
+
